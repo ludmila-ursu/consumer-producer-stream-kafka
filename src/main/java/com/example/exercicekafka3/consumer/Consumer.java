a4954e2249db31d43eccec3ccfa7ab3467dd8fb7
Léa <38650627+am3thyste@users.noreply.github.com>
2021-02-11 22:27:10 +0100

    added comments

--- src/main/java/com/example/exercicekafka3/consumer/Consumer.java
+++ src/main/java/com/example/exercicekafka3/consumer/Consumer.java
@@ -18,6 +18,7 @@ public class Consumer {
 
 	int i = 0;
 	public void consumerSubscription() {
+		//define properties for the Consumer:
 		Properties props = new Properties();
 		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
 		props.put(ConsumerConfig.GROUP_ID_CONFIG, "groupIdExample");
@@ -25,12 +26,9 @@ public class Consumer {
 		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "io.confluent.kafka.serializers.KafkaAvroDeserializer");
 		props.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://localhost:8081");
 		
+		//create a new Consumer and subscribe to a Topic
 		try (KafkaConsumer<String, DataFile> consumer = new KafkaConsumer<>(props)) {
-			System.out.println("dans le try de consummer");
-
 			consumer.subscribe(Arrays.asList("my_avro_topic"));
-			System.out.println("après le subscribe du consommateur");
-
 			
 			}
 		}
